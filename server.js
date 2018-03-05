70fd2a54c660ee85adc20a8711ff110cbe792e5b
pushpakumara.14@cse.mrt.ac.lk <pushpakumara.14@cse.mrt.ac.lk>
2018-03-05 21:40:58 +0530

    adding some features

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var app = express();
 
+app.set('view engine', 'ejs');
+
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(__dirname + '/public'));
+
 app.listen(80, function() {
     console.log('Chatfuel Bot-Server listening on port 80...');
 });
@@ -9,4 +14,11 @@ app.get('/*', function(req, res) {
     var jsonResponse = [];
     jsonResponse.push({ "text": "Hi. " + (Math.random() * 5 + 1).toFixed(0) + " is a lucky number..." });
     res.send(jsonResponse);
+});
+
+// set the home page route
+app.get('/', function(req, res) {
+
+    // ejs render automatically looks in the views folder
+    res.render('index');
 });
