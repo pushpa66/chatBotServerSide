0885cafd0c113ac3170c29227156ee33775cb754
pushpakumara.14@cse.mrt.ac.lk <pushpakumara.14@cse.mrt.ac.lk>
2018-03-05 21:50:47 +0530

    testing

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
+
 app.set('view engine', 'ejs');
 
 // make express look in the public directory for assets (css/js/img)
@@ -21,4 +25,8 @@ app.get('/', function(req, res) {
 
     // ejs render automatically looks in the views folder
     res.render('index');
+});
+
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
 });
