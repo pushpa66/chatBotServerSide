6b6ae8b3847cbdb7a02cd8b56fdd61fcd445053b
pushpakumara.14@cse.mrt.ac.lk <pushpakumara.14@cse.mrt.ac.lk>
2018-03-05 23:12:00 +0530

    testing

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ app.listen(80, function() {
 
 app.get('/*', function(req, res) {
     var jsonResponse = [];
-    jsonResponse.push({ "text": "Hi. " + (Math.random() * 5 + 1).toFixed(0) + " is a lucky number..." });
+    jsonResponse.push({ "myText": "Hi. " + (Math.random() * 5 + 1).toFixed(0) + " is a lucky number..." });
     res.send(jsonResponse);
 });
