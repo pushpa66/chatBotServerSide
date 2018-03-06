72888e1e80e1d069ce0ab38e47fb4c4dbb7749ac
pushpakumara.14@cse.mrt.ac.lk <pushpakumara.14@cse.mrt.ac.lk>
2018-03-06 14:45:37 +0530

    test

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-// var express = require('express');
-// var app = express();
-//
-// app.listen(80, function() {
-//     console.log('Chatfuel Bot-Server listening on port 80...');
-// });
-//
-// app.get('/', function(req, res) {
-//     res.setHeader('Content-Type', 'application/json');
-//     res.send(JSON.stringify({ name: "pushpa kumara" }));
-// });
-var express = require("express");
-var router = express.Router();
+var express = require('express');
+var app = express();
 
-router.get("/", function(req, res, next) {
-    var jsonData = {"name": "Pushpa Kumara"};
-    res.json(jsonData);
+app.listen(80, function() {
+    console.log('Chatfuel Bot-Server listening on port 80...');
 });
 
-module.exports = router;
+app.get('/', function(req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({ name: "pushpa kumara" }));
+});
+// var express = require("express");
+// var router = express.Router();
+//
+// router.get("/", function(req, res, next) {
+//     var jsonData = {"name": "Pushpa Kumara"};
+//     res.json(jsonData);
+// });
+//
+// module.exports = router;
