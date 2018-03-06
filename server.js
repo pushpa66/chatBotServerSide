809e5e4a3ad6acb3e721b8a0afb45b7617eb0cca
pushpakumara.14@cse.mrt.ac.lk <pushpakumara.14@cse.mrt.ac.lk>
2018-03-06 15:39:30 +0530

    test

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ app.get('/', function (req, res) {
     res.setHeader('Content-Type', 'application/json');
     res.send(JSON.stringify({ name: 'Pushpa Kumara' }));
 });
-app.listen(3000, function () {
+app.listen(80, function () {
     console.log('Example app listening on port 3000!');
 });
